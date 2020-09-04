d8bbcd271129de2d22a8c3c42bd6340ce094db9f
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-09-04 09:48:21 -0600

    Starting project with some views

---
+++ App.js
@@ -0,0 +1,27 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+//Screens
+import LoginScreen from './screens/LoginScreen';
+import ChatScreen from './screens/ChatScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Chat" component={ChatScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
